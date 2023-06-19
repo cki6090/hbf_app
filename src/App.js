caa93de98c57170bbcad8d393f077164ccd9da80
tom <52525423+cki6090@users.noreply.github.com>
2023-06-19 16:20:09 +0900

    20230619

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          로고
+          HAPPY BABY FOOD
+
+          로그인 
+          1. 아기 사진을 올려주세요
+          2. 아기 이름 나이 성별 
+          3. 아기 알러지체크
+          4. 1일 - 몇끼 (가격)
+          5. 주 - 몇일 (가격)
+          6. 주수 선택 - 시작일 선택123
+
+          완료 페이지
+          사진 이름 나이
+          알러지 
+          1일 3끼 주 3회분 
+          맞으면 ok
+
+          -인트로 이동
+          메인 이유식 정보 +  내용 + 사진
+          섹션 2 - 유아 용품
+          섹션 3 - 간식 
+
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
