af2fa00ebfcb60a07ef8e8e150b3647fe3c2a6f3
cki6090 <52525423+cki6090@users.noreply.github.com>
2023-08-17 14:55:56 +0900

    230817

--- src/components/component/search_box.js
+++ src/components/component/search_box.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function search_box() {
-    return (
-        <div className='search_box'>
-            <div>
-                <span class="material-symbols-outlined">
-                    search
-                </span>
-            </div>
-            <div><input placeholder='검색어를 입력해 주세요.'></input></div>
-        </div>
-    )
+  return (
+    <div className="search_box">
+      <div>
+        <span class="material-symbols-outlined">search</span>
+      </div>
+
+      <div>
+        <input placeholder="검색어를 입력해 주세요."></input>
+      </div>
+    </div>
+  );
 }
 
-export default search_box
+export default search_box;
